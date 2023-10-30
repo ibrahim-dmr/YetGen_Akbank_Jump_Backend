17571b0a2df5e1151c630c1023e11c0b6df7bd1e
İbrahim Halil DEMİR <ibrahimdmr1103@gmail.com>
2023-10-30 11:41:50 +0300

    file paths updated

---
+++ EncapsulationExcercise/Entities/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExcercise.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        //public decimal Balance { get; private set; }
+
+        private decimal balance;  //private olan mutalaka camelCase olmalı
+
+        public decimal Balance   //Public olan mutlaka PascalCase olmalı
+        {
+            get { return balance; }
+            set 
+            { 
+                if(Math.Abs(value - balance) <= 500)
+                balance = value;
+                Console.WriteLine($"Current Value: {balance}");
+            }
+        }
+
+
+        public Account(string name, string surname)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            Balance = 2000;
+        }
+
+        //public void Withdraw(decimal amount)
+        //{
+        //    if (Balance - amount >= 0)
+        //        Balance -= amount;
+        //    else
+        //        Console.WriteLine("Not enough money");
+        //}
+
+    }
+}
---
+++ EncapsulationExcercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using EncapsulationExcercise.Entities;
+
+Console.WriteLine("Encapsulation");
+
+Account account1 = new("İbrahim", "Demir");
+//account1.Withdraw(2000);
+//account1.Withdraw(200);
+//account1.Withdraw(200);
+account1.Balance -= 1000;
+account1.Balance -= 500;
+account1.Balance -= 450;
+
+
+Console.WriteLine($"Balance: {account1.Balance}");
