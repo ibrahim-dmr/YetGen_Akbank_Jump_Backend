7fb4cd7965429e641e1ca19d5fa7a9a8c72e3191
İbrahim Halil DEMİR <ibrahimdmr1103@gmail.com>
2023-11-02 23:28:09 +0300

    file paths updated

---
+++ LinqMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Linq Methods!");
+
+List<int> numbers = new() { 100, 45, 140, 150, 250 };
+
+bool AllItemsGreater = numbers.All(x => x > 50);
+
+//bool allItemsGreater = true;
+
+//foreach (int number in numbers)
+//{
+//    if (number <= 50)
+//    {
+//        allItemsGreater = false;
+//        break;
+//    }
+//}
+
+Console.WriteLine(AllItemsGreater);
