faa275e1005d103c7914d416968f1079d0009d20
İbrahim Halil DEMİR <ibrahimdmr1103@gmail.com>
2023-11-03 19:13:10 +0300

    file paths updated

---
+++ EntityFrameWork_InMemory/Persistence/NoteMasterDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EntityFrameWork_InMemory.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWork_InMemory.Persistence
+{
+    public class NoteMasterDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("NoteMaster");
+        }
+    }
+}
---
+++ EntityFrameWork_InMemory/Program.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameWork_InMemory.Persistence;
+using EntityFrameWork_InMemory.Entities;
+
+Console.WriteLine("Entity Framework Core - InMemory");
+
+NoteMasterDbContext _context = new();
+
+_context.Notes.Add(new("Note1"));
+_context.Notes.Add(new("Note2"));
+_context.Notes.Add(new("Note3"));
+_context.Notes.Add(new("Note4"));
+
+_context.SaveChanges();
+
+List<Note> notes = _context.Notes.ToList();
+
+Console.WriteLine();
+
+
